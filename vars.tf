98ecfcfeaedb0f475e3467c97cbb3466ba5d4120
Charles <charles.zhao.sg@gmail.com>
2022-10-13 17:47:49 +0800

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,2 +1,7 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_ACCESS_KEY" {}
+variable "ENVIRONMENT" {}
+variable "IAM_INSTANCE_PROFILE" {}
+variable "KEY_PAIR" {}
+variable "SUBNET_ID" {}
+variable "VPC_SECURITY_GROUP_IDS" {}
